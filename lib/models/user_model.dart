5c747db8badbdc1da508c9f9856f89808b49b45b
kishore kumawath <kishorekumawath6@gmail.com>
2025-06-18 00:48:11 +0530

    created the models for user

---
+++ lib/models/user_model.dart
@@ -0,0 +1,65 @@
+class UserResponse {
+  final int page;
+  final int perPage;
+  final int total;
+  final int totalPages;
+  final List<UserModel> data;
+
+  UserResponse({
+    required this.page,
+    required this.perPage,
+    required this.total,
+    required this.totalPages,
+    required this.data,
+  });
+
+  factory UserResponse.fromJson(Map<String, dynamic> json) {
+    return UserResponse(
+      page: json['page'],
+      perPage: json['per_page'],
+      total: json['total'],
+      totalPages: json['total_pages'],
+      data: (json['data'] as List)
+          .map((user) => UserModel.fromJson(user))
+          .toList(),
+    );
+  }
+}
+
+class UserModel {
+  final int id;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String avatar;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
+  });
+
+  String get fullName => '$firstName $lastName';
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      email: json['email'],
+      firstName: json['first_name'],
+      lastName: json['last_name'],
+      avatar: json['avatar'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'first_name': firstName,
+      'last_name': lastName,
+      'avatar': avatar,
+    };
+  }
+}
