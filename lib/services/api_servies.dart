37eb9f429ee43fbf0e8acfc478173b1d2b262c8b
kishore kumawath <kishorekumawath6@gmail.com>
2025-06-18 12:15:53 +0530

    changed the api service url logic

--- lib/services/api_servies.dart
+++ lib/services/api_servies.dart
@@ -1,24 +1,41 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/user_model.dart';
+import 'package:dio/dio.dart';
 
 class ApiService {
-  static const String baseUrl = 'https://reqres.in/api';
-
-  static Future<UserResponse> fetchUsers({int page = 1}) async {
+  static const String baseUrl = 'https://randomuser.me/api';
+  static final Dio _dio = Dio(BaseOptions(
+    baseUrl: baseUrl,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 10),
+  ));
+  
+  static Future<UserResponse> fetchUsers({int results = 10}) async {
     try {
-      final response = await http.get(
-        Uri.parse('$baseUrl/users?page=$page'),
-        headers: {'Content-Type': 'application/json'},
-      );
-
+      final response = await _dio.get('/', queryParameters: {'results': results});
+      
+      print('Status Code: ${response.statusCode}');
+      print('Response Data: ${response.data}');
+      
       if (response.statusCode == 200) {
-        final jsonData = json.decode(response.body);
-        return UserResponse.fromJson(jsonData);
+        return UserResponse.fromJson(response.data);
       } else {
         throw Exception('Failed to load users: ${response.statusCode}');
       }
+    } on DioException catch (e) {
+      print('Dio Error: ${e.message}');
+      print('Error Type: ${e.type}');
+      if (e.response != null) {
+        print('Error Status: ${e.response?.statusCode}');
+        print('Error Data: ${e.response?.data}');
+      }
+      throw Exception('Network error: ${e.message}');
     } catch (e) {
+      print('General Error: $e');
       throw Exception('Network error: $e');
     }
   }
